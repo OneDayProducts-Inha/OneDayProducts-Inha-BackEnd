4a3baed6bdc411a21740850a47748e956d6483f0
chaerlo127 <monynony0203@gmail.com>
2023-11-21 01:36:26 +0900

    #1 feat: 프로젝트 초기 설정

---
+++ src/main/java/inha/app/MyGate/MyGateApplication.java
@@ -0,0 +1,13 @@
+package inha.app.MyGate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGateApplication.class, args);
+	}
+
+}
