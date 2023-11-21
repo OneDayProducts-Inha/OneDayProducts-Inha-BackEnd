dc0ccdd762af149bc6dc37bdad02796a7e858bf5
Eunkyy <melon0802@naver.com>
2023-11-22 03:30:09 +0900

    #4 fix: toString 추가

--- src/main/java/inha/app/MyGate/community/dto/response/PostRes.java
+++ src/main/java/inha/app/MyGate/community/dto/response/PostRes.java
@@ -2,9 +2,11 @@ package inha.app.MyGate.community.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class PostRes {
     private String title;
     private String content;
