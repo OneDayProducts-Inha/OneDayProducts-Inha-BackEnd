8d9c5bea583b83cf64ba22c59f11f24f36c5e20e
Eunkyy <melon0802@naver.com>
2023-11-21 17:34:15 +0900

    feat:스웨거 초기 설정

---
+++ src/main/java/inha/app/MyGate/config/SwaggerConfig.java
@@ -0,0 +1,89 @@
+package inha.app.MyGate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration	// 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
+@EnableSwagger2	// Swagger2를 사용하겠다는 어노테이션
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "MyGate API 명세서",
+                "MyGate API 명세서",
+                "1.0",
+                "Terms of service",
+                new Contact("Sallo Szrajbman", "www.baeldung.com", "salloszraj@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    //리소스 핸들러 설
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    // API마다 구분짓기 위한 설정.
+    @Bean
+    public Docket productApi() {
+        return getDocket("유저", Predicates.or(
+                PathSelectors.regex("/user.*")));
+    }
+
+    @Bean
+    public Docket commApi() {
+        return getDocket("커뮤니티", Predicates.or(
+                PathSelectors.regex("/community.*")));
+
+    }
+
+    @Bean
+    public Docket allApi() {
+        return getDocket("전체", Predicates.or(
+                PathSelectors.regex("/*.*")));
+    }
+
+    //swagger 설정.
+    public Docket getDocket(String groupName, Predicate<String> predicate) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(groupName)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cc.kr"))
+                .paths(predicate)
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+
+    //swagger ui 설정.
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .displayRequestDuration(true)
+                .validatorUrl("")
+                .build();
+    }
+
+}
