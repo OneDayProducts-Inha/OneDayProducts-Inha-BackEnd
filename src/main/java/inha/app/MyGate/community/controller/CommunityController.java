03c81a4f3c4102651ce6e4720c990dfb36f40898
Eunkyy <melon0802@naver.com>
2023-11-22 03:17:05 +0900

    #4 feat: 커뮤니티 글 작성

--- src/main/java/inha/app/MyGate/community/controller/CommunityController.java
+++ src/main/java/inha/app/MyGate/community/controller/CommunityController.java
@@ -1,10 +1,14 @@
 package inha.app.MyGate.community.controller;
 
+import inha.app.MyGate.common.Exception.BaseResponse;
+import inha.app.MyGate.community.dto.request.PostReq;
+import inha.app.MyGate.community.service.CommunityService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static inha.app.MyGate.common.Exception.BaseResponseStatus.SUCCESS;
 
 @Api(tags = "커뮤니티", value = "커뮤니티 관리 기능 구현한 community Controller 입니다.")
 @Slf4j
@@ -12,4 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/community")
 public class CommunityController {
+    private final CommunityService communityService;
+
+    //글 작성
+    @ResponseBody
+    @PostMapping("/post")
+    public BaseResponse<String> createPost(@RequestBody PostReq postReq){
+        try{
+            String title = communityService.createPost(postReq);
+            System.out.println(title);
+            return new BaseResponse<>(SUCCESS);
+        }catch (Exception e){
+            return new BaseResponse<>(e.getMessage());
+        }
+    }
 }
--- src/main/java/inha/app/MyGate/community/service/CommunityService.java
+++ src/main/java/inha/app/MyGate/community/service/CommunityService.java
@@ -1,9 +1,26 @@
 package inha.app.MyGate.community.service;
 
+import inha.app.MyGate.community.dto.request.PostReq;
+import inha.app.MyGate.community.entity.Community;
+import inha.app.MyGate.community.repository.CommunityRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Service
 public class CommunityService {
+    private final CommunityRepository communityRepository;
+
+    public Community save(Community community) {
+        return communityRepository.save(community);
+    }
+    @Transactional
+    public String createPost(PostReq postReq) {
+        communityRepository.save(postReq.toEntity());
+
+        return postReq.getTitle();
+    }
 }
