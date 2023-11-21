c7d51726ba4386c4874bd38b1a2a3225c5ef7d56
chaerlo127 <monynony0203@gmail.com>
2023-11-22 04:18:32 +0900

    #7 feat: 내가 작성한 게시글

--- src/main/java/inha/app/MyGate/community/repository/CommunityRepository.java
+++ src/main/java/inha/app/MyGate/community/repository/CommunityRepository.java
@@ -1,12 +1,15 @@
 package inha.app.MyGate.community.repository;
 
 import inha.app.MyGate.community.entity.Community;
+import inha.app.MyGate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     Optional<Community> findByCommunityIdAndStatus(Long communityId, Boolean status);
+    List<Community> findByUserAndStatus(User user, Boolean status);
 }
