7d29e2fd3a06ad57b834f390416794fb2c18dc4e
Eunkyy <melon0802@naver.com>
2023-11-23 13:53:31 +0900

    #34 feat: 로그인 유저 액세스 수정

--- src/main/java/inha/app/MyGate/community/dto/request/PostReq.java
+++ src/main/java/inha/app/MyGate/community/dto/request/PostReq.java
@@ -2,6 +2,7 @@ package inha.app.MyGate.community.dto.request;
 
 
 import inha.app.MyGate.community.entity.Community;
+import inha.app.MyGate.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +18,12 @@ public class PostReq {
     private String category;
 
 
-    public Community toEntity() {
+    public Community toEntity(User user) {
         Community communityEntity = Community.builder()
                 .title(title)
                 .content(content)
                 .category(category)
+                .user(user)
                 .build();
         return communityEntity;
     }
