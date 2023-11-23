75eaeecab17faae71985f3c5f75663c96533267a
chaerlo127 <monynony0203@gmail.com>
2023-11-23 12:44:02 +0900

    feat: cors 설정 추가

--- src/main/java/inha/app/MyGate/common/config/WebConfig.java
+++ src/main/java/inha/app/MyGate/common/config/WebConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
